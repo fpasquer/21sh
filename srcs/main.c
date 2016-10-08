5635c98f9c7d90b2ffc7a691db93fe9d0a62c71d
JCFC <chen.jchristophe@gmail.com>
2016-10-08 19:56:03 +0200

    redirection ./21sh -ls -la fonctionne

--- srcs/main.c
+++ srcs/main.c
@@ -1,8 +1,27 @@
 #include "21sh.h"
 #include <stdio.h>
+#include <fcntl.h>
 
-int main(void)
+int main(int argc, char **argv, char **env)
 {
-	printf("%s\n", ":)");
+	int pid = fork();
+
+	if (argc > 1) {
+		if (pid < 0) {
+			perror("error");
+			exit (-1);
+		}
+		if (pid == 0) {
+			printf("child\n");	
+			int fw = open("test.txt", O_WRONLY);
+			dup2(fw, 1);
+			execve("/bin/ls", &argv[1], env);
+		}
+		else {
+			printf("parent\n");
+			wait(NULL);
+		}
+	}
+	printf("%d\n", pid);
 	return (0);
 }
