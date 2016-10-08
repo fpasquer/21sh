c3da87bd0c3c370963a74c60e688773b60269294
JCFC <chen.jchristophe@gmail.com>
2016-10-08 17:19:12 +0200

    clean project

--- incs/21sh.h
+++ incs/21sh.h
@@ -6,7 +6,7 @@
 /*   By: fpasquer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/08 08:58:42 by fpasquer          #+#    #+#             */
-/*   Updated: 2016/10/08 10:30:59 by fpasquer         ###   ########.fr       */
+/*   Updated: 2016/10/08 17:14:14 by jchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,24 +20,6 @@
 #include "../libft/libft.h"
 
 #define ERROR -1
-
-/*
-**	**path :		path tableau a 2 dimensions des paths
-**	*path_line:		variable env recu dans le main
-*/
-
-typedef struct				s_21sh
-{
-	char					**path;
-	char					*path_line;
-}							t_21sh;
-
-/*
-**	init_21sh.h
-*/
-t_21sh						*init_21sh(char **env);
-t_21sh						*get_21sh(t_21sh *shell_21sh);
-int							del_21sh();
 
 
 #endif
