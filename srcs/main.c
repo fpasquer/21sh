dda351e14c6977ec157185ddf17361644d5d275e
JCFC <chen.jchristophe@gmail.com>
2016-10-09 20:31:12 +0200

    test << mais ne fonctionne pas

--- srcs/main.c
+++ srcs/main.c
@@ -2,17 +2,25 @@
 
 int main(int argc, char **argv, char **env)
 {
-	int pid = fork();
-	if (pid < 0) {
-		perror("ERROR FORK");
+	int pid = 0;
+
+	if (argc > 1) {
+		pid = fork();
+		if (pid < 0) {
+			perror("ERROR FORK");
+		}
+		else if (pid != 0) {
+			printf("parent\n");
+			wait(NULL);
+		}
+		else {
+			printf("child\n");
+			int fd = open("test.txt", O_WRONLY);
+			int test = dup2(fd, 2);
+			//printf("%d = test, %d = fd\n", test, fd);
+			//close(0);
+			execve("/bin/ls", &argv[1], env);
+		}
 	}
-	else if (pid != 0) {
-		printf("parent\n");
-		wait(NULL);
-	}
-	else {
-		printf("child\n");
-	}
-	printf("%d\n", pid);
 	return (0);
 }
