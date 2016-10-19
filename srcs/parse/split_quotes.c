6b174bed44b262ed529cda573de513a7052c0ca3
JCFC <chen.jchristophe@gmail.com>
2016-10-19 18:21:13 +0200

    modification d'un nom d'une fonction

--- srcs/parse/split_quotes.c
+++ srcs/parse/split_quotes.c
@@ -38,7 +38,7 @@ static char *ft_strsub_bis(char *str, int index, int end, int nb_ht) {
 	return(new_str);
 }
 
-static char *malloc_string_to_space(char *str, int *index, char delimiter) {
+static char *parse_string(char *str, int *index, char delimiter) {
 	
 	if (ft_isalpha(str[*index])) {
 		delimiter = CHAR_SPACE;
@@ -46,11 +46,8 @@ static char *malloc_string_to_space(char *str, int *index, char delimiter) {
 	else {
 		(*index)++;
 	}
-	
-
 	int start_index = (*index);
 	int nb_ht = 0;
-
 	while (str[*index] && str[*index] != delimiter) {
 		if ((str[*index] == CHAR_S_QUOTE || str[*index] == CHAR_W_QUOTE) && delimiter == CHAR_SPACE) {
 			delimiter = str[*index];
@@ -82,7 +79,7 @@ char **split_quotes(char *str) {
 	char *tmp_str = ft_strdup(str);
 	while (i < len) {
 		if (tmp_str[i] != CHAR_SPACE) {
-				new_str = malloc_string_to_space(tmp_str, &i, str[i]);
+				new_str = parse_string(tmp_str, &i, str[i]);
 				list = ft_add_to_array(new_str, list);
 		}
 		i++;
@@ -90,62 +87,3 @@ char **split_quotes(char *str) {
 	free(tmp_str);
 	return (list);
 }
-
-
-// static char **malloc_quotes(char *str, int *index, char **list, char delimiter) {
-// 	char **new_list = NULL;
-// 	(*index)++;
-// 	int start_index = (*index);
-
-// 	while (str[*index] && str[*index] != delimiter) {
-// 		(*index)++;
-// 	}
-// 	char *new_str = ft_strsub(str, start_index, (*index) - start_index);
-// 	new_list = ft_add_to_array(new_str, list);
-// 	return (new_list);
-// }
-
-// static char **malloc_string_to_space(char *str, int *index, char **list, char delimiter) {
-// 	char **new_list = NULL;
-// 	int start_index = (*index);
-
-// 	while (str[*index] && str[*index] != delimiter) {
-// 		if ((str[*index] == CHAR_S_QUOTE || str[*index] == CHAR_W_QUOTE) && delimiter == CHAR_SPACE) {
-// 			delimiter = str[*index];
-// 			str[*index] = CHAR_HT;
-// 		}
-// 		(*index)++;
-// 		if ((str[*index] == CHAR_S_QUOTE && delimiter == CHAR_S_QUOTE) || 
-// 				(str[*index] == CHAR_W_QUOTE && delimiter == CHAR_W_QUOTE)) {
-// 			str[*index] = CHAR_HT;
-// 			delimiter = CHAR_SPACE;
-// 			(*index)++;
-// 		}
-// 	}
-// 	char *new_str = ft_strsub(str, start_index, (*index) - start_index);
-// 	new_list = ft_add_to_array(new_str, list);
-// 	return (new_list);
-// }
-
-// char **split_quotes(char *str) {
-// 	int i = 0;
-// 	int len = ft_strlen(str);
-// 	char **new_list = NULL;
-	
-// 	if (str == NULL) {
-// 		return NULL;
-// 	}
-// 	char *tmp_str = ft_strdup(str);
-// 	while (i < len) {
-// 		if (tmp_str[i] != CHAR_SPACE) {
-// 			if (tmp_str[i] == CHAR_W_QUOTE || tmp_str[i] == CHAR_S_QUOTE) {
-// 				new_list = malloc_quotes(tmp_str, &i, new_list, tmp_str[i]);
-// 			}
-// 			else {
-// 				new_list = malloc_string_to_space(tmp_str, &i, new_list, CHAR_SPACE);
-// 			}
-// 		}
-// 		i++;
-// 	}
-// 	return (new_list);
-// }
