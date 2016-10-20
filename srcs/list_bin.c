7eb68072d01d3a5aaaa8eca5a0ba059e40d2769d
Florian PASQUEREAU <fpasquer@e1r4p4.42.fr>
2016-10-20 08:56:34 +0200

    add slash for directs.

--- srcs/list_bin.c
+++ srcs/list_bin.c
@@ -6,7 +6,7 @@
 /*   By: fpasquer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/19 17:10:58 by fpasquer          #+#    #+#             */
-/*   Updated: 2016/10/19 18:55:06 by fpasquer         ###   ########.fr       */
+/*   Updated: 2016/10/20 08:55:24 by fpasquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,13 +39,42 @@ static int					count_objets(char *path, char *begin_name)
 	return (count);
 }
 
+static int					check_file(char ***list, char *path, char *name,
+		int i)
+{
+	char					*tmp_path;
+	struct stat				stat;
+
+	if (list == NULL || path == NULL || name == NULL || i < 0)
+		return (ERROR);
+	if (ft_strcmp(name, ".") != 0 && ft_strcmp(name, "..") != 0)
+	{
+		if ((tmp_path = ft_strjoin(path, name)) == NULL)
+			return (ERROR);
+		ft_bzero(&stat, sizeof(stat));
+		if (lstat(tmp_path, &stat) == 0)
+		{
+			if ((stat.st_mode & S_IFMT) == S_IFDIR)
+				list[0][i] = ft_strjoin(name, "/");
+			else
+				list[0][i] = ft_strdup(name);
+			if (list[0][i] == NULL)
+				return (ERROR);
+		}
+		ft_memdel((void**)&tmp_path);
+		return (true);
+	}
+	return (false);
+}
+
 static char					**save_list(char ***list, char *path,
 		char *begin_name)
 {
 	int						i;
+	int						ret;
 	size_t					len;
 	DIR						*dirent;
-	struct dirent			*str_dirent;
+	struct dirent			*str_dir;
 
 	if (list == NULL || path == NULL || begin_name == NULL)
 		return (NULL);
@@ -55,12 +84,13 @@ static char					**save_list(char ***list, char *path,
 	i = 0;
 	if ((dirent = opendir(path)) != NULL)
 	{
-		while ((str_dirent = readdir(dirent)) != NULL)
-			if (len == 0 ||
-					ft_strncmp(str_dirent->d_name, begin_name, len) == 0)
+		while ((str_dir = readdir(dirent)) != NULL)
+			if (len == 0 || ft_strncmp(str_dir->d_name, begin_name, len) == 0)
 			{
-				if ((list[0][i++] = ft_strdup(str_dirent->d_name)) == NULL)
-					break;
+				if ((ret = check_file(list, path, str_dir->d_name, i))
+						== ERROR)
+					break ;
+				i = (ret == false) ? i : i + 1;
 			}
 		closedir(dirent);
 	}
