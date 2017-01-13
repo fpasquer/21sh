257e890ecb5a50b735937f1643daad640fb2ab63
Capocci Florent <fcapocci@Capoccis-Mac.local>
2017-01-13 19:50:46 +0100

    word_left ok

--- srcs/keybord/event_move2.c
+++ srcs/keybord/event_move2.c
@@ -6,7 +6,7 @@
 /*   By: fcapocci <fcapocci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/12 18:34:56 by fcapocci          #+#    #+#             */
-/*   Updated: 2017/01/13 00:03:10 by fcapocci         ###   ########.fr       */
+/*   Updated: 2017/01/13 19:48:33 by fcapocci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,28 +26,16 @@ int				word_left(void)
 {
 	t_line		*c;
 
-	if ((c = g_lines) == NULL)
+	if ((c = g_curs) == NULL)
 		return (false);
-	if (c->curs == NULL || c->curs->next == NULL)
+	if (c->curs == NULL || c->curs == NULL)
 		return (false);
-	if (c->curs->next->c == 9 || c->curs->next->c == 32)
-	{
-		while ((c->curs && c->curs->next && c->curs->next->c == 9)
-		|| (c->curs && c->curs->next && c->curs->next->c == 32))
-		{
+	if (c->curs->c == ' ')
+		while (c->curs && c->curs->c == ' ')
 			move_left();
-			c = g_lines;
-		}
-	}
 	else
-	{
-		while ((c->curs && c->curs->next && c->curs->next->c != 9)
-		|| (c->curs && c->curs->next && c->curs->next->c != 32))
-		{
+		while (c->curs && c->curs->c != ' ')
 			move_left();
-			c = g_lines;
-		}
-	}
 	return (true);
 }
 
@@ -55,27 +43,15 @@ int				word_right(void)
 {
 	t_line		*c;
 
-	if ((c = g_lines) == NULL)
+	if ((c = g_curs) == NULL)
 		return (false);
 	if (c->curs == NULL || c->curs->next == NULL)
 		return (false);
-	if (c->curs->next->c == 9 || c->curs->next->c == 32)
-	{
-		while ((c->curs && c->curs->next && c->curs->next->c == 9)
-		|| (c->curs && c->curs->next && c->curs->next->c == 32))
-		{
+	if (c->curs->next->c == ' ')
+		while (c->curs && c->curs->next && c->curs->next->c == ' ')
 			move_right();
-			c = g_lines;
-		}
-	}
 	else
-	{
-		while ((c->curs && c->curs->next && c->curs->next->c != 9)
-		|| (c->curs && c->curs->next && c->curs->next->c != 32))
-		{
+		while (c->curs && c->curs->next && c->curs->next->c != ' ')
 			move_right();
-			c = g_lines;
-		}
-	}
 	return (true);
 }
