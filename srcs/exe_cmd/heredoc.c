57f53f6d6c2c8d94ab50612b2b427c51faae08b5
Florent CAPOCCI <fcapocci@e1r4p13.42.fr>
2017-05-10 19:29:53 +0200

    start heredoc

--- srcs/exe_cmd/heredoc.c
+++ srcs/exe_cmd/heredoc.c
@@ -6,11 +6,27 @@
 /*   By: fcapocci <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/09 16:01:48 by fcapocci          #+#    #+#             */
-/*   Updated: 2017/05/09 16:02:33 by fcapocci         ###   ########.fr       */
+/*   Updated: 2017/05/10 19:29:24 by fcapocci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int				heredoc()
+#include "../../incs/shell_21sh.h"
+#include "../../incs/key.h"
+
+static char				get_char(void)
 {
+	char				b[SIZE_BUFF];
+
+	if (read(STDIN_FILENO, b, SIZE_BUFF) <= 0)
+		return (ERROR);
+	if ((b[0] >= 32 && b[0] <= 126) || ENTER)
+		return (b[0]);
+	return (0);
+}
+
+int						heredoc(char *token)
+{
+	if (token != NULL)
+		return (0);
 	return (0);
 }
